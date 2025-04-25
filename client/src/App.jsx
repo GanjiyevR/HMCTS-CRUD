bc2a59adaec6acd289b7f87cd1f36fd3911bba7d
GanjiyevR <96064338+GanjiyevR@users.noreply.github.com>
2025-04-25 10:10:32 +0100

    Route creation + Placeholders

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,23 @@
 import { useState } from 'react'
 import './App.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Tasks from './Tasks'
+import CreateTask from './CreateTask'
+import UpdateTask from './UpdateTask'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     
+     <div>
+      <BrowserRouter>
+        <Routes paths="/" element={ <Tasks />}></Routes>
+        <Routes paths="/create" element={<CreateTask />}></Routes>
+        <Routes paths="/update" element={<UpdateTask />}></Routes>
+      </BrowserRouter>
+     </div>
     </>
   )
 }
---
+++ client/src/UpdateTask.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function UpdateTask () {
+    return (
+        <div>
+            Update Tasks
+        </div>
+    )
+}
+
+export default UpdateTask;
