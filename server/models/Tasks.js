748f08689a79805e43779238709704e595d0f9da
GanjiyevR <96064338+GanjiyevR@users.noreply.github.com>
2025-04-25 13:32:17 +0100

    Update Tasks.js
    Added some schema validation

--- server/models/Tasks.js
+++ server/models/Tasks.js
@@ -1,10 +1,20 @@
 const mongoose = require('mongoose')
 
 const TaskSchema = new mongoose.Schema({
-    title: String,
+    title: {
+        type: String,
+        required: true,
+    },
     description: String,
-    status: String,
-    dueDate: Date
+    status: {
+        type: String,
+        enum: ['Incomplete', 'In Progress', 'Complete'],
+        default: 'Incomplete'
+    },
+    dueDate: {
+        type: Date,
+        required: true,
+    }
 })
 
 const TaskModel = mongoose.model("tasks", TaskSchema)
